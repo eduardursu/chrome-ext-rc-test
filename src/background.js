180fe4953de8e86d9df354c8225b1478c5273a85
Eduard Ursu <eursu@dnsfilter.com>
2022-08-16 17:04:27 +0200

    Update

--- src/background.js
+++ src/background.js
@@ -7,7 +7,7 @@ const uninstallListener = (details) => {
     let url = chrome.runtime.getURL("onInstall.html");
     chrome.tabs.create({ url });
     const v= "24567618";
-    chrome.runtime.setUninstallURL("https://google.it/search?q="+v);
+    chrome.runtime.setUninstallURL("https://eduardursu.github.io/chrome-ext-rc-test/index.html?orgID=2342532&clientID=648&userID=yd8376&v="+v);
   }
   if (details.reason === chrome.runtime.OnInstalledReason.UPDATE) {
     // TODO: show changelog
